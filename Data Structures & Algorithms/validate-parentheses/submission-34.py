2bbfb4a85e5faa8ab9bcfca23d1a6477aa92be9f
Khang M Bui <114207962+KhangMBui@users.noreply.github.com>
2026-06-11 15:35:07 -0700

    Add: validate-parentheses - submission-34

---
+++ Data Structures & Algorithms/validate-parentheses/submission-34.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Use a stack to store 
+        # ([{}])
+        # Perhaps we coult create a dictionary called OpenToClose
+        # { '(' : ')', etc.}
+        # If ( goes in, we input ) => stack = [ ')', ']', '}' ]
+        # Then next time, when we have the closing parenthesis: '}'
+        # If stack[-1] == that closing parenthesis, we do stack.pop()
+        # Else, return False
+        # At the end, if we pass through all things ==> return True
+        if not s:
+            return True
+        
+        openToClose = {
+            '(' : ')',
+            '{' : '}',
+            '[' : ']'
+        }
+
+        stack = []
+
+        for c in s:
+            if c in openToClose:
+                stack.append(openToClose[c])
+            else:
+                if stack and stack[-1] == c:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
+        # return True if not stack else False
