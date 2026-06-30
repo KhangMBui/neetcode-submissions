0a6f762715cc57a3a2721e0bbcea2b09768ec66b
Khang M Bui <114207962+KhangMBui@users.noreply.github.com>
2026-06-29 20:15:59 -0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,53 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not numCourses or not prerequisites:
+            return True
+        
+        # Benefit from DFS perhaps
+        # Firstly, create a dictionary: { course : [prereqs]}
+        # { 2: [1], 1: [0, 0.5]}, etc.
+
+        # We need to detect a cycle 
+        # (bc it'd be impossible to complete courses involved in the cycle)
+
+
+        pre_map = defaultdict(list)
+        for course, prereq in prerequisites:
+            pre_map[course].append(prereq)
+
+        # We iterate over each course, run a DFS from
+        # that course, and first try to finish its prereq courses
+        # by recursively traversing through them
+        # To detect a cycle, we initialize a hash set called path,
+        # which contains the nodes visited in the current DFS call
+        # If we encounter a course already in path, we can conclude that
+        # a cycle is detected
+        path = set()
+
+        def dfs(course: int) -> bool:
+            if course in path: # cycle detected
+                return False
+            # We reached a point where there's no
+            # prerequisites
+            if not pre_map[course]:
+                return True
+            
+            # Add the current course to the set:
+            path.add(course)
+            
+            pre_course = pre_map[course]
+
+            for pre in pre_course:
+                if not dfs(pre):
+                    return False
+            
+            # Done checking this course's prerequisites:
+            path.remove(course)
+
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        
+        return True
